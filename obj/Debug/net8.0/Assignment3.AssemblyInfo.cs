9e214460321407744a7914790b9fcc40c70cbfe3
Jeeva Vijayaraghavan <jvijayaraghavan3936@conestogac.on.ca>
2024-07-28 21:30:38 -0400

    Final Commits

--- obj/Debug/net8.0/Assignment3.AssemblyInfo.cs
+++ obj/Debug/net8.0/Assignment3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87c967d55763226e7a02736a3e856fb920588b97")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
